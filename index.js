18b231630166a85c042be6ab4a1d005f9944a232
AMutaa <piusandru@gmail.com>
2018-09-27 15:29:39 -0400

    string count

--- index.js
+++ index.js
@@ -43,3 +43,20 @@ function foo2() {
   };
 }
 console.log(foo2());
+
+var carname = "Volvo xc60"
+
+console.log(carname.toLowerCase());
+
+// Count number of letters is a string
+function count(params, string) {
+  var count = 0;
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === params) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(count("p", "apple"));
